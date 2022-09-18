047601e208d8f2ae8ba452bb47474b8ead570454
braverokmc79 <braverokmc79@gmail.com>
2022-09-18 16:32:21 +0900

    21.로또 번호 생성기

--- String/a.js
+++ String/a.js
@@ -1,1 +1,8 @@
-git 
+//ES2015 string 에 새로운 메서들.
+
+let str = "hello world ! ^^ ~~";
+let matchstr = "^ ~~";
+
+console.log(str.startsWith(matchstr));
+console.log(str.endsWith(matchstr));
+console.log("includes  test :", str.includes("world"));
