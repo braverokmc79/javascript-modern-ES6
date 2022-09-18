03e391d66606a62e5145402f6269ca442557186f
braverokmc79 <braverokmc79@gmail.com>
2022-09-19 06:32:25 +0900

    38.step4-1. XHR통신

--- minproject/src/main.js
+++ minproject/src/main.js
@@ -2,7 +2,25 @@
 class Blog {
     constructor() {
         console.log("Blog is start !");
+        const dataURL = "https://jsonplaceholder.typicode.com/photos?_start=0&_end=10";
+        this.setInitData(dataURL);
     }
+
+    setInitData(dataURL) {
+        this.getData(dataURL);
+    }
+
+    async getData(dataURL) {
+        const list = await fetch(dataURL).then(res => res.json()).then(data => data);
+        console.log(" list type :", typeof list);
+        list.map((v) => {
+            console.log(v.title);
+            return v;
+        })
+
+    }
+
+
 }
 
 export default Blog;
