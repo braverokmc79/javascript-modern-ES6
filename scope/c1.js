72c4fcd8b92ab62d394cee4d7ffbe3c474727b79
braverokmc79 <braverokmc79@gmail.com>
2022-09-18 11:09:55 +0900

    3.const - 선언된 변수 지키기

---
+++ scope/c1.js
@@ -0,0 +1,18 @@
+// function home() {
+//     var homename = "my house";
+//     homename = "your house";
+//     console.log(homename);
+// }
+
+function home() {
+    const homename = "my house";
+    homename = "your house";
+    console.log(homename);
+}
+//const 를 기본으로 사용한다.
+//그런데 변경이 될 수 있는 변수는 let을 사용한다.
+//var 는 사용하지 않는다.
+
+
+
+home()
