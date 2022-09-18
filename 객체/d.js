9cdeb9c2475dc92191538399367c9cd0512df2c6
braverokmc79 <braverokmc79@gmail.com>
2022-09-18 21:24:16 +0900

    30.Object setPrototypeOf로 객체만들기

---
+++ 객체/d.js
@@ -0,0 +1,41 @@
+
+//Object setPrototypeOf로 객체만들기
+
+const healthObj = {
+    showHealth: function () {
+        console.log("오늘 운동시간 : " + this.healthTime);
+    },
+
+    setHealth: function (newTime) {
+        this.healthTime = newTime;
+    }
+}
+
+
+// const myHealth = Object.assign(Object.create(healthObj), {
+//     name: "crong",
+//     lastTim: "11:20"
+// });
+
+
+const newObj = Object.setPrototypeOf({
+    name: "crong",
+    lastTim: "11:20"
+}, healthObj);
+
+console.log("myHealth is", newObj);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
