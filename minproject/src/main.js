3fcd145c6f2e4ef69cb92e6a013d20cc01004937
braverokmc79 <braverokmc79@gmail.com>
2022-09-19 13:53:05 +0900

    40.step5. Set자료에 데이터 추가(찜하기기능)

--- minproject/src/main.js
+++ minproject/src/main.js
@@ -1,9 +1,49 @@
 
 class Blog {
     constructor() {
-        console.log("Blog is start !");
+        this.setInitVariables();
+        this.registerEvents();
+        this.likedMap = new Map();
+    }
+
+    setInitVariables() {
+        this.blogList = document.querySelector(".blogList > ul");
+    }
+
+
+    registerEvents() {
         const dataURL = "https://jsonplaceholder.typicode.com/photos?_start=0&_end=10";
-        this.setInitData(dataURL);
+        const startBtn = document.querySelector(".start");
+
+        startBtn.addEventListener("click", () => {
+            const li = document.querySelectorAll(".blogList li");
+            if (li.length === 0) this.setInitData(dataURL);
+            else this.blogList.innerHTML = "";
+
+        })
+
+        this.blogList.addEventListener("click", ({ target }) => {
+            const targetClassName = target.className;
+            console.log("targetClassName : ", targetClassName);
+
+            //like 버튼 클릭시 속성값 데이터를 가져온 후에 Set 저장한다.
+            if (targetClassName === "like") {
+                const setData = {
+                    albumId: target.getAttribute("data-id"),
+                    id: target.getAttribute("data-id"),
+                    title: target.getAttribute("data-title"),
+                    url: target.getAttribute("data-url"),
+                    thumbnailUrl: target.getAttribute("data-url")
+                }
+                this.likedMap.set("key-" + setData.id, setData);
+
+                this.likedMap.forEach((value, key) => {
+                    console.log("key : ", key, value);
+                })
+
+            }
+
+        });
     }
 
     setInitData(dataURL) {
@@ -17,11 +57,14 @@ class Blog {
     }
 
     insertPosts(list) {
-        const ul = document.querySelector(".blogList > ul");
+
         list.map((item) => (
-            ul.innerHTML += `<li><a href=${item.url}>
-            <img src="${item.thumbnailUrl}">
-            ${item.title}</a >
+            this.blogList.innerHTML += `
+            <li>
+             <a href=${item.url}><img src="${item.thumbnailUrl}"></a >
+                ${item.title}
+                <button class="like" data-id="${item.id}" data-title="${item.title}"
+                    data-url="${item.url}" data-thumbnailUrl="${item.thumbnailUrl}">찜하기</button>
             </li > `
         ));
 
