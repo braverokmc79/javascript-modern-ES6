c46dd3e9ee18698dfa716016cc956c767afa7dde
braverokmc79 <braverokmc79@gmail.com>
2022-09-19 15:13:21 +0900

    41.step6. 찜목록뷰 업데이트

--- minproject/src/main.js
+++ minproject/src/main.js
@@ -7,7 +7,11 @@ class Blog {
     }
 
     setInitVariables() {
+        //fetch 코드스쿼드에서  가져오는   dataLlist
+        this.dataLlist = null;
         this.blogList = document.querySelector(".blogList > ul");
+        this.likeList = document.querySelector(".like-list > ul");
+
     }
 
 
@@ -22,45 +26,100 @@ class Blog {
 
         })
 
+        //코드스쿼드 블로그에서 찜버튼 클릭시 이벤트 처리
         this.blogList.addEventListener("click", ({ target }) => {
-            const targetClassName = target.className;
-            console.log("targetClassName : ", targetClassName);
+            this.likeAndUnLikeButtonEvent(target);
+        });
 
-            //like 버튼 클릭시 속성값 데이터를 가져온 후에 Set 저장한다.
-            if (targetClassName === "like") {
-                const setData = {
-                    albumId: target.getAttribute("data-id"),
-                    id: target.getAttribute("data-id"),
-                    title: target.getAttribute("data-title"),
-                    url: target.getAttribute("data-url"),
-                    thumbnailUrl: target.getAttribute("data-url")
-                }
-                this.likedMap.set("key-" + setData.id, setData);
-
-                this.likedMap.forEach((value, key) => {
-                    console.log("key : ", key, value);
-                })
-
-            }
-
+        //찜목록 에서 버튼 클릭시 이벤트 처리
+        this.likeList.addEventListener("click", ({ target }) => {
+            this.likeAndUnLikeButtonEvent(target);
         });
     }
+
+    likeAndUnLikeButtonEvent(target) {
+        const targetClassName = target.className;
+        // console.log("targetClassName : ", targetClassName);
+
+        //like 버튼 클릭시 속성값 데이터를 가져온 후에 Set 저장한다.
+        if (targetClassName === "like") {
+            const setData = {
+                albumId: target.getAttribute("data-id"),
+                id: target.getAttribute("data-id"),
+                title: target.getAttribute("data-title"),
+                url: target.getAttribute("data-url"),
+                thumbnailUrl: target.getAttribute("data-url")
+            }
+            //찜 목록에 추가
+            this.likedMap.set("key-" + setData.id, setData);
+
+            //찜 된 목록(div)의 클래스를 like 에서 unlike 로 변경하기.
+            target.className = "unlike";
+            target.innerText = "찜취소";
+
+            //내찜목록 뷰에 추가
+            this.updateLikedList();
+
+        }
+
+        if (targetClassName === "unlike") {
+            //찜 취소를 클릭한 경우에, 찜하기로 다시 변경하고, 찜목을 제거하고, 찜 목로뷰로 랜더링한다.
+            target.className = "like";
+            target.innerText = "찜하기";
+            const id = target.getAttribute("data-id");
+            // console.log("찜 취소 키 값 : ", key);
+            this.likedMap.delete("key-" + id);
+
+            this.dataLlist.map((item) => {
+                if (item.id == id) {
+                    console.log(" 찜취소 아이디 : ", id);
+                    const unLikeTarget = document.querySelector(`#blogList-${id} .unlike`);
+                    unLikeTarget.className = "like";
+                    unLikeTarget.innerText = "찜하기";
+                }
+            });
+
+            //내찜목록 뷰에 제거
+            this.updateLikedList();
+        }
+    }
+
+
+    //내찜목록 
+    updateLikedList() {
+        let likedSum = "";
+
+        Array.from(this.likedMap).map((value) => {
+            console.log(value);
+            const item = value[1];
+            likedSum += `
+            <li id="likeList-${item.id}">
+             <a href=${item.url}><img src="${item.thumbnailUrl}"></a >
+                ${item.title}
+                <button class="unlike" data-id="${item.id}" data-title="${item.title}"
+                    data-url="${item.url}" data-thumbnailUrl="${item.thumbnailUrl}">찜취소</button>
+            </li > `
+        });
+
+        this.likeList.innerHTML = likedSum;
+    }
+
 
     setInitData(dataURL) {
         this.getData(dataURL);
     }
 
     async getData(dataURL) {
-        const list = await fetch(dataURL).then(res => res.json()).then(data => data);
-        this.insertPosts(list);
+        this.dataLlist = await fetch(dataURL).then(res => res.json()).then(data => data);
+        this.insertPosts();
 
     }
 
-    insertPosts(list) {
+    insertPosts() {
 
-        list.map((item) => (
+        this.dataLlist.map((item) => (
             this.blogList.innerHTML += `
-            <li>
+            <li id="blogList-${item.id}">
              <a href=${item.url}><img src="${item.thumbnailUrl}"></a >
                 ${item.title}
                 <button class="like" data-id="${item.id}" data-title="${item.title}"
